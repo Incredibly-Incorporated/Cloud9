bca3cc4070a8ee503e10dc7abdbebe02e6ea6c76
Incredibly-Incorporated <impian0611@gmail.com>
2016-02-14 13:09:24 -0500

    Version 0.0.6
    Changelog:
    - Updated launch.bat to automatically determine working directory.
    - Updated Screen library for better performance in existing functions.
    - Updated Reference file to match directory changes and additions.
    - Updated _env-beta directory for new library testing environment.
    - Removed various null.txt placeholders.
    - Added Text library.
    > This library has been heavily documented to describe how the functions
    work, and how to properly invoke them.
    > Testing has also begun on testing the functions within this library.
    - Added 'center' function to Text library.
    - Added 'display' function to Text library.
    - Added small Ascii program in cloud9 directory to act as a shortcut to
    hop straight to the AsciiFinder program.

--- launch.bat
+++ launch.bat
@@ -1,26 +1,14 @@
 @ECHO OFF
 COLOR f0
 
-IF EXIST D:\Documents\GitHub\Cloud9 (
-	ECHO Found Repository on D
+IF EXIST %CD% (
+	ECHO Found Repository: %CD%
 	TIMEOUT 3 /nobreak
-	java -Dcom.xtansia.ccemu.path="D:\Documents\GitHub\Cloud9" -jar ccemuredux-launcher-1.8.9.jar
+	java -Dcom.xtansia.ccemu.path=%CD% -jar ccemuredux-launcher-1.8.9.jar
 ) ELSE (
-	ECHO Repository NOT Found on D
-)
-
-TIMEOUT 3 /nobreak
-
-IF EXIST E:\Documents\GitHub\Cloud9 (
-	ECHO Found Repository on E
-	TIMEOUT 3 /nobreak
-	java -Dcom.xtansia.ccemu.path="E:\Documents\GitHub\Cloud9" -jar ccemuredux-launcher-1.8.9.jar
-) ELSE (
-	ECHO Repository NOT Found on E
+	ECHO There was an error in determining working directory!
 	ECHO Unable to Determine Drive
-	PAUSE
 )
 
-TIMEOUT 3 /nobreak
 ECHO Session was terminated by User
 PAUSE
